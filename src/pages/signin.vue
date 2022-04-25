469a0c3b14e6af07bf4bc7f42f525417715ee4c7
kannalbtech <kanna@lbtech.ac.th>
2022-04-25 14:15:04 +0700

    Attribue binding

---
+++ src/pages/signin.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <form>
+      <div>
+        Sign-in
+      </div>
+      <div>
+        <label for="username">Username</label>
+        <input type="text" id="username" :value="User1">
+      </div>
+      <div>
+        <label for="password">Password</label>
+        <input type="password" id="password" :value="Pass1">
+      </div>
+      <div>
+        <input type="checkbox" id="remember" :checked="check1">
+        <label for="remember">Remember username?</label>
+      </div>
+      <div>
+        <button type="button" :disabled="lock">
+          Sign-In
+        </button>
+        <button type="reset" :disabled="lock">
+          Reset
+        </button>
+      </div>
+    </form>
+  </div>
+</template>
+
+<script>
+ export default {
+   data(){
+     return {
+       User1:'kanchana',
+       Pass1:'1234',
+       check1:true,
+       lock:false, 
+     }
+   }
+ }
+</script>
--- src/pages/template.vue
+++ src/pages/template.vue
@@ -1,5 +1,38 @@
 <template>
-  <div>Hello 1</div>
-  <div>Hello 2</div>
+// attribue bind => v-bind:ชื่อแอตทริบิวต์="ชื่อตัวแปร"
+// element bind => {{ชื่อตัวแปร}}
+// v-html สั่งให้แสดงผลตาม html ในตัวแปร
+
+  <div class="text-bold" :title="name">
+    Hello {{name.toUpperCase()}}
+    อายุ: {{age}}
+    สถานศึกษา : {{school}}
+  </div>
+  <div>Hello 2 <span v-html="name"></span></div>
+
+  <button :disabled="lock">
+    test <b>{{name}}</b>
+  </button>
 </template>
- 
+
+ //-----------------------------------
+ <script>
+    export default{
+      data(){
+        return {
+          name:'kanchana',
+          // name2:'<i>kanchana</i>',
+          age:25,
+          school:'วิทยาลัยเทคนิคลพบุรี',
+          lock:true,//true or false
+        }
+
+      }
+
+    }
+ </script>
+
+ //-----------------------------------
+ <style>
+
+ </style>
